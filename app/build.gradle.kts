44a07cf924bccb5df9eb157f4732de7e24bab213
Shankara130 <galby.alayubi130@gmail.com>
2026-01-13 06:48:14 +0700

    Add DrawingActivity with layout and mock functionality;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -34,7 +32,6 @@ android {
         jvmTarget = "11"
     }
     buildFeatures {
-        compose = true
         dataBinding = true
         viewBinding = true
     }
@@ -55,14 +52,13 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
 
-    // Room Database with KSP (KSP for Room works!)
+    // Room Database
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
 
-    // Image Loading (use KAPT for Glide if using annotations)
+    // Image Loading
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    kapt("com.github.bumptech.glide:compiler:4.16.0")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
