5bf4f9a108c6d5b7982d3222ce6408cf9939a605
Shankara130 <galby.alayubi130@gmail.com>
2026-01-12 13:55:57 +0700

    Add MainActivity with navigation setup and floating action button functionality

---
+++ app/src/main/java/com/example/artistic/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.artistic.ui.main
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.artistic.R
+import com.example.artistic.databinding.ActivityMainBinding
+import com.example.artistic.ui.drawing.DrawingActivity
+
+class MainActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityMainBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        setupNavigation()
+        setupFAB()
+    }
+    
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        
+        binding.bottomNav.setupWithNavController(navController)
+    }
+    
+    private fun setupFAB() {
+        binding.fabCreate.setOnClickListener {
+            startActivity(Intent(this, DrawingActivity::class.java))
+        }
+    }
+}
