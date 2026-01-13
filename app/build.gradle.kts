a3d84b623b65135d6fbb058640c023ae0d8edb1d
Shankara130 <galby.alayubi130@gmail.com>
2026-01-13 12:33:56 +0700

    Add Material and Navigation dependencies in Gradle configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     
-    // Material3
+    // Material
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     
     // Compose BOM
@@ -65,6 +66,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.compose)
     
     // Room
     implementation(libs.androidx.room.runtime)
